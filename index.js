e30b15d2c51b4fe32879e705f484442e4b15a673
MohammedAlmanea <MohammedAlmanea@outlook.com>
2022-09-28 15:38:47 +0300

    Used destructuring method for importing second-util in index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const firstUtil = require('./utilities/first-util');
-const secondUtil = require('./utilities/second-util');
+//Using destructuring method
+const { lgNum, cut3 } = require('./utilities/second-util');
 
 let array1 = [1, 2, 3, 4, 5, 6];
 let array2 = [9, 8, 7];
@@ -9,5 +10,5 @@ let array4 = ['T', 'h', 'e', 'r', 'e'];
 console.log(`Using sum()\n${firstUtil.sum(array2)}`);
 console.log(`Using concat()\n${firstUtil.concat(array3, array4)}`);
 
-console.log(`Using lgNum()\n${secondUtil.lgNum(array2)}`);
-console.log(`Using cut3()\n${secondUtil.cut3(array1)}`);
+console.log(`Using lgNum()\n${lgNum(array2)}`);
+console.log(`Using cut3()\n${cut3(array1)}`);
